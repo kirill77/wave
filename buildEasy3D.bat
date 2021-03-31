6e9cb5dd3e0ab5586adc0b695a31d28e92dd57f8
Kirill Dmitriev <dkirill@nvidia.com>
2021-03-31 09:03:34 -0700

    sync with particular version of easy3d (because otherwise i get their recent bugs)

--- buildEasy3D.bat
+++ buildEasy3D.bat
@@ -1,7 +1,13 @@
-git clone https://github.com/LiangliangNan/Easy3D.git --recurse-submodules
-mkdir Easy3D\build
+pushd ..
 
-pushd
-cd Easy3D\build
+git clone https://github.com/LiangliangNan/Easy3D.git --recurse-submodules
+cd Easy3D
+
+git checkout a8a82e3132ef2aab747b328103822d74f7ae1bc5
+git submodule update --init --recursive
+
+mkdir build
+cd build
 cmake ..
+
 popd
